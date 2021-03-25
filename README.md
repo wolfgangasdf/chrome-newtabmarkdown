ac6eaa444bab5029a05fc255f9d1cf979302c732
wolfgang <wolfgang.loeffler@gmail.com>
2021-03-25 17:12:34 +0100

    ini commit

---
+++ README.md
@@ -0,0 +1,25 @@
+
+# chrome-newtabmarkdown
+
+Chrome extension that overrides Chrome's new tab page with a markdown notebook, based on the beautiful https://github.com/maxbeier/chrome-blank-tab but it renders markdown using [marked](https://github.com/markedjs/marked) (loaded only when needed). Why? Easy hyperlinks. It's still fast, simple, customizable.
+
+- <kbd>Escape</kbd> - toggle between editor (html below it) and html only, the document is saved if the editor is closed. 
+- You can also double-click the html to open the editor and jump to the current heading.
+- <kbd>Command</kbd> or <kbd>Ctrl</kbd> + <kbd>s</kbd> - download a backup.
+- You might need to click the page to make the hotkeys work.
+- It synchronizes if open in multiple tabs.
+
+Homepage: https://github.com/wolfgangasdf/chrome-newtabmarkdown
+
+
+# Screenshot
+
+![Screenshot](https://raw.githubusercontent.com/wolfgangasdf/chrome-newtabmarkdown/master/screenshot.png)
+
+## Installation
+
+Download or clone this repo, navigate to *[`chrome://extensions/`](chrome://extensions/)* , enable developer mode, click on `Load unpacked`, and select the downloaded folder (unzip before if you have downloaded a zip). Don't remove the folder!
+
+## Used libraries
+* https://github.com/markedjs/marked
+
---
+++ script.js
@@ -0,0 +1,120 @@
+var md = document.getElementById("md");
+var mdd = document.getElementById("mdd");
+var ht = document.getElementById("ht");
+
+async function markedIniAndRender() { // add source pos tags for headings to marked.js
+   await import(chrome.runtime.getURL("marked.min.js"));
+
+   // this stores original tokenizer stuff which can be called. Otherwise, rules are not initialized.
+   var origlexer = new marked.Lexer({ ...marked.defaults, tokenizer: new marked.Tokenizer() });
+
+   const tokenizer = {
+      heading(src) { // add source position tag
+         var res = origlexer.tokenizer.heading(src);
+         if (res) res.charno = md.value.length - src.length;
+         return res;
+      },
+      paragraph(src) { // add source position tag
+         var res = origlexer.tokenizer.paragraph(src);
+         if (res) res.charno = md.value.length - src.length;
+         return res;
+      },
+   };
+   marked.use({ tokenizer });
+
+   const walkTokens = (token) => {
+      if (token.charno) { // append charno tag token!
+         var ct = { type: 'html', text: '<div hidden class="tag" data-charno=' + token.charno + '></div>' };
+         if (token.tokens) { token.tokens.push(ct); } else { token.tokens = [ct]; }
+      }
+   };
+   marked.use({ walkTokens });
+
+   renderit();
+}
+
+function renderit() {
+   ht.innerHTML = marked(md.value);
+}
+
+function updateht() {
+   if (typeof marked !== "function") { // dynamically load marked
+      markedIniAndRender();
+   } else
+      renderit();
+};
+
+function updatemd(s) {
+   md.value = s;
+   updateht();
+};
+
+function toggleEditor(showonly = false) {
+   if (!showonly && mdd.style.display === "block") { // close editor, save
+      localStorage.setItem('notes', md.value);
+      localStorage.setItem('notes-html', ht.innerHTML);
+      mdd.style.display = "none";
+   } else { // edit
+      mdd.style.display = "block";
+      md.focus();
+   }
+};
+
+// startup: load saved or README.md
+var iniht = localStorage.getItem('notes-html');
+var inimd = localStorage.getItem('notes');
+if (iniht != null && inimd != null) {
+   md.value = inimd;
+   ht.innerHTML = iniht;
+} else {
+   var url = chrome.runtime.getURL("README.md");
+   fetch(url).then((response) => response.text().then((text) => updatemd(text)));
+}
+
+mdd.onkeyup = () => { // update html on edit
+   updateht();
+};
+
+ht.ondblclick = function (e) { // double click: find source position
+   var curr = e.target || e.srcElement;
+   toggleEditor(true);
+   var pos = 0;
+   while (curr) {
+      if (curr.className === 'tag') { pos = curr.dataset.charno }
+      else if (child = curr.querySelector('.tag')) pos = child.dataset.charno;
+      if (pos) {
+         break;
+      }
+      next = curr.nextElementSibling; // code tag node is after clicked node
+      if (next) { curr = next } else { curr = curr.parentNode }; // or go up?
+   }
+   md.focus();
+   md.setSelectionRange(pos, pos);
+   md.blur();
+   md.focus();
+};
+
+window.onstorage = (event) => { // Inter Tab Synchronization
+   if (event.key === "notes") md.value = event.newValue
+   else if (event.key === "notes-html") ht.innerHTML = event.newValue;
+};
+
+// Key Events
+document.onkeydown = (event) => {
+   if (event.code === 'Tab') { // Change Tab to Spaces
+      event.preventDefault();
+      const spaces = '  ';
+      document.execCommand('insertText', false, spaces);
+   } else if (event.code === 'Escape') { // toggle editor
+      toggleEditor();
+   } else if ((event.metaKey || event.ctrlKey) && event.key === 's') { // backup
+      event.preventDefault();
+      var elem = window.document.createElement('a');
+      elem.download = "chrome-newtabmarkdown-backup-" + new Date().toISOString() + ".md";
+      elem.href = window.URL.createObjectURL(new Blob([md.value], { type: "text/plain" }));
+      document.body.appendChild(elem);
+      elem.click();
+      document.body.removeChild(elem);
+      mdd.style.display = "none";
+   }
+};
